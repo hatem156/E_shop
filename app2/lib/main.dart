3f4ee8bee47f0db0899363e9913400a1efee8650
hatem156 <126658469+hatem156@users.noreply.github.com>
2023-03-29 07:06:54 +0200

    warp materialapp with new widget

--- app2/lib/main.dart
+++ app2/lib/main.dart
@@ -1,6 +1,8 @@
 
 import 'package:app1/pages/login.dart';
+import 'package:app1/provider/cart.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 
 void main() {
@@ -11,11 +13,14 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData.light(),
-      home:   const Login(),
+    return ChangeNotifierProvider(
+      create: (context) {return Cart();},
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Flutter Demo',
+        theme: ThemeData.light(),
+        home:   const Login(),
+      ),
     );
   }
 }
