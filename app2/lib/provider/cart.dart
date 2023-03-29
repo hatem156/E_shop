9f8911fb7651e606994be18afb1165e71eb75d37
hatem156 <126658469+hatem156@users.noreply.github.com>
2023-03-29 07:05:13 +0200

    create new file called cart

--- app2/lib/provider/cart.dart
+++ app2/lib/provider/cart.dart
@@ -1,8 +1,5 @@
 import 'package:flutter/material.dart';
 
 class Cart with ChangeNotifier {
-  // create new properties & methods
   String myname = " Hatem";
-
-// use "notifyListeners();" at the end of every method
 }
