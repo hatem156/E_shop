b3a931db072f0de677ae95aa76a73934661aadae
hatem156 <126658469+hatem156@users.noreply.github.com>
2023-03-29 07:04:05 +0200

    create new folder called provider

---
+++ app2/lib/provider/cart.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class Cart with ChangeNotifier {
+  // create new properties & methods
+  String myname = " Hatem";
+
+// use "notifyListeners();" at the end of every method
+}
