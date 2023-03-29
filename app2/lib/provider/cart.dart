2467a9ed89658669a09d8a86190bcfec0ea0bd7b
hatem156 <126658469+hatem156@users.noreply.github.com>
2023-03-29 10:18:42 +0200

    create cart file and create class inside it

--- app2/lib/provider/cart.dart
+++ app2/lib/provider/cart.dart
@@ -1,5 +1,19 @@
+import 'package:app1/widgets/item.dart';
 import 'package:flutter/material.dart';
 
 class Cart with ChangeNotifier {
-  String myname = " Hatem";
+  List selectitems=[
+  
+  ];
+  double coast = 0;
+  add(item product){
+    selectitems.add(product);
+    coast +=product.price;
+    notifyListeners();
+  }
+  delete(item product){
+    selectitems.remove(product);
+    coast -=product.price;
+    notifyListeners();
+  }
 }
